fb5fdd34770b9bd1bcacae76d492d7b3e4b30e6c
Krishna <krishnakevadiya32@gmail.com>
2023-06-04 12:00:17 +0530

    product

--- index.php
+++ index.php
@@ -6,9 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
 </head>
 <body>
-    dgk testing
+
+   <?php 
+   include "./Components/Navbar.php"
+
+   ?>
+   <?php 
+   include "./Components/Slider.php"
+
+   ?>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </body>
 </html>
